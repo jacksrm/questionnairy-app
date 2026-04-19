c2434b13fba0421533d4549caf8f42744edcd0ef
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-04-19 16:06:26 -0300

    Pre enviroment change

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1,8 +1,10 @@
-export enum commands {
-  get_all_patients = 'get_all_patients',
-  get_patient = 'get_patient',
-  delete_patient = 'delete_patient',
-  create_patient = 'create_patient',
-  update_patient = 'update_patient',
-  get_patient_by_name = 'get_patient_by_name',
-}
+const commands = {
+  get_all_patients: 'get_all_patients',
+  get_patient: 'get_patient',
+  delete_patient: 'delete_patient',
+  create_patient: 'create_patient',
+  update_patient: 'update_patient',
+  get_patient_by_name: 'get_patient_by_name',
+};
+
+export { commands };
